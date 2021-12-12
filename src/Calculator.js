bdd56544ff9d8d5587e0b4751c157a6d88809493
Marwan tamer <marwanmarwan536@gmail.com>
2021-12-12 21:11:34 +0200

    f

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
 class Gauss_Calculator extends React.Component {
+    state={
+        mSize: 0,
+        Xs: [],
+        equations: []
+    } 
     render() {
-      return <h2>Hi, I am a Car!</h2>;
+      return <div className="flex flex-col flex-auto">
+          <h1> السلام عليكم و رحمة الله و بركاته</h1>
+          
+          <div className="bg-slate-500 flex flex-row">
+            <input id="mSize" type="number" defaultValue="0" min="0"></input>
+            <button type="button" onClick={()=>(this.setState({mSize: document.querySelector('#mSize').value}))}>
+                Click Me!</button>
+          </div>
+          
+          <div></div>
+          
+          <div></div>
+      </div>;
     }
   }
 
